026430750b315acd80e9803db559355320ca44a7
kirill pavlov <2020kirillp@gmail.com>
2023-11-14 13:50:05 -0800

    random grabbing from good site

--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,36 @@
 // Tracking time on page
 const redirects = [
-	"https://github.com/SimplifyJobs/Summer2024-Internships",
-	"https://neetcode.io/roadmap",
+    "https://github.com/SimplifyJobs/Summer2024-Internships",
+    "https://neetcode.io/roadmap",
+    "test1",
+    "test2",
+    "test3",
+    "test4"
 ];
 
 function trackTime() {
-	document.addEventListener("DOMContentLoaded", () => {
-		const startTime = new Date().getTime();
+    document.addEventListener("DOMContentLoaded", () => {
+        const startTime = new Date().getTime();
 
-		window.addEventListener("beforeunload", () => {
-			const endTime = new Date().getTime();
+        window.addEventListener("beforeunload", () => {
+            const endTime = new Date().getTime();
 
-			const totalTime = (endTime - startTime) / 1000;
+            const totalTime = (endTime - startTime) / 1000;
 
-			console.log(totalTime);
-		});
-	});
+            console.log(totalTime);
+        });
+    });
 }
 
 function blurScreen() {
-	if (totalTime === 15) {
-		// activate a css site
-	}
+    if (totalTime === 15) {
+        // activate a css site
+    }
 }
 
-function redirect() {}
+function redirect() {
+    const redirectIndex = Math.floor(Math.random() * redirects.length);
+    return redirects[redirectIndex];
+}
+
+console.log(redirect());
